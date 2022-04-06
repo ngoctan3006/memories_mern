a590bb31d0aa7ed2c2521d7b38cbbf66fee351e1
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-06 23:45:41 +0700

    add update post api

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -23,6 +24,26 @@ export const createPost = async (req, res) => {
         res.status(201).json(newPost);
     } catch (error) {
         res.status(409).json({
+            message: error.message
+        });
+    }
+};
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send('No post found with that ID');
+
+    try {
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+            new: true
+        });
+
+        res.json(updatedPost);
+    } catch (error) {
+        res.status(404).json({
             message: error.message
         });
     }
