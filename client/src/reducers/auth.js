1de2db90f95044c7bcd707e41d3f7594c757d252
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-12 22:44:05 +0700

    fix bug syntax

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,7 +3,7 @@ import { AUTH, LOGOUT } from '../constants/actionTypes';
 const authReducer = (state = { authData: null }, action) => {
     switch (action.type) {
         case AUTH:
-            localStorage.setItem('profile', JSON.stringify(...action?.payload));
+            localStorage.setItem('profile', JSON.stringify({ ...action?.payload }));
             return { ...state, authData: action?.payload };
         case LOGOUT:
             return state;
