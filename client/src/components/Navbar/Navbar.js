8302193cb4ba4576268f4cdc45d76318bca9a559
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-13 16:13:40 +0700

    fix bug syntax

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
         const token = user?.token;
         if (token) {
             const decodedToken = decode(token);
-            if (decodedToken.exp * 1000 < new Date.getTime()) logout();
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
         }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
