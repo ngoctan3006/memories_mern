1e73b8aadccb3e0b4e8447dccb013939e50faf12
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-13 14:52:43 +0700

    fix bug import

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, signup } from '../controllers/users';
+import { signin, signup } from '../controllers/users.js';
 
 const router = express.Router();
 
