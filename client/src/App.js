4bb21487c5d32f5f8ae2bc57bede348c509d5236
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-14 14:07:12 +0700

    add post details component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
     return (
@@ -11,7 +12,10 @@ const App = () => {
             <Container maxWidth='lg'>
                 <Navbar />
                 <Switch>
-                    <Route exact path='/' component={Home} />
+                    <Route exact path='/' component={() => <Redirect to='/posts' />} />
+                    <Route exact path='/posts' component={Home} />
+                    <Route exact path='/posts/search' component={Home} />
+                    <Route exact path='/posts/:id' component={PostDetails} />
                     <Route exact path='/auth' component={Auth} />
                 </Switch>
             </Container>
