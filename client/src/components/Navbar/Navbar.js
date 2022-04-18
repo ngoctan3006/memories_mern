8d2ac1e6dec7d53d039fca262cacb3a87fccdf34
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-18 10:36:57 +0700

    change logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,8 @@ import decode from 'jwt-decode';
 import { useDispatch } from 'react-redux';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import useStyles from './styles';
-import memories from '../../images/memories.png';
+import memoriesLogo from '../../images/memoriesLogo.png';
+import memoriesText from '../../images/memoriesText.png';
 import { LOGOUT } from '../../constants/actionTypes';
 
 const Navbar = () => {
@@ -33,18 +34,15 @@ const Navbar = () => {
 
     return (
         <AppBar className={classes.appBar} position='static' color='inherit'>
-            <div className={classes.brandContainer}>
-                <Typography
-                    component={Link}
-                    to='/'
-                    className={classes.heading}
-                    variant='h2'
-                    align='center'
-                >
-                    Memories
-                </Typography>
-                <img className={classes.image} src={memories} alt='memories' height='60'></img>
-            </div>
+            <Link to='/' className={classes.brandContainer}>
+                <img src={memoriesText} alt='icon' height='45px' />
+                <img
+                    className={classes.image}
+                    src={memoriesLogo}
+                    alt='memories'
+                    height='40px'
+                ></img>
+            </Link>
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
