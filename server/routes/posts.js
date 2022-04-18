a2ad6fab6b67410403b7e49502b6b34150088799
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-04-18 10:34:50 +0700

    add search post api

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { createPost, deletePost, getPosts, likePost, updatePost } from '../controllers/posts.js';
+import {
+    createPost,
+    deletePost,
+    getPosts,
+    getPostsBySearch,
+    likePost,
+    updatePost
+} from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
